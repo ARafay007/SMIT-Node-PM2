67cd97af60fcefdf81f3c6fe35b3e04149747f37
ARafay007 <rafay_007@outlook.com>
2026-08-01 10:27:35 +0500

    testing PM2

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/block', (req, res) => {
+    const start = Date.now();
+
+    while (Date.now() - start < 3000) {
+        // Block event loop for 5 seconds
+    }
+
+    res.send(`Finished blocking ${process.pid}`);
+});
+
+app.get('/', (req, res) => {
+    res.send(`Finished response ${process.pid}`);
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
+});
